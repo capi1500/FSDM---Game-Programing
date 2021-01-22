f8031bcc53356e04d054b197af6bbe48fdac020a
Kacper Chętkowski <k.chetkowski@fsdm.edu.pl>
2021-01-22 13:48:51 +0100

    Pong

--- src/common.cpp
+++ src/common.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <SFML/System.hpp>
+#include <cmath>
 
 double distance(const sf::Vector2f& a, const sf::Vector2f& b){
 	return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
