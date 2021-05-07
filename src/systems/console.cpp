6a689682a3ae3be42cf1214b2fab282113f36a71
mitos <fenix.warld@gmail.com>
2021-05-07 18:13:58 +0200

    Colouring console output

--- src/systems/console.cpp
+++ src/systems/console.cpp
@@ -1,14 +1,18 @@
 #include "console.hpp"
 #include <iostream>
 
+#ifdef WIN32
+#include <windows.h>
+#endif
+
 Signal<Message> message;
 
 void Console::onNotify(const Message& event){
 	if(types.count(event.type) == 1){
 		if(event.type == Message::Debug)
-			std::clog << "DEBUG: " + event.getMessage() << "\n";
+			std::clog << "\033[1;34mDEBUG: " + event.getMessage() << "\033[0m\n";
 		if(event.type == Message::Error)
-			std::cerr << "ERROR: " + event.getMessage() << "\n";
+			std::cerr << "\033[1;31mERROR: " + event.getMessage() << "\033[0m\n";
 	}
 }
 
@@ -24,6 +28,20 @@ void Console::listenType(Message::Type type){
 }
 
 Console::Console(){
+#ifdef WIN32 // we need this only when on windows, because on unix-like systems it is enabled by default
+	HANDLE stdo = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD dwMode = 0;
+	GetConsoleMode(stdo, &dwMode);
+	dwMode |= 0x0004;
+	SetConsoleMode(stdo, dwMode);
+
+	HANDLE stde = GetStdHandle(STD_ERROR_HANDLE);
+	dwMode = 0;
+	GetConsoleMode(stde, &dwMode);
+	dwMode |= 0x0004;
+	SetConsoleMode(stde, dwMode);
+#endif
+
 	message.addListener(this);
 	gameEventSignal.addListener(this);
 }
